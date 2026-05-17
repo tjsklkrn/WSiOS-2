15e26d95a2c3a129b824a56da127f0219dea957c
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2026-05-17 19:19:31 +0530

    new

--- ws-hackathon-app-release/WSHackathonApp/Core/Constants/AppConstants.swift
+++ ws-hackathon-app-release/WSHackathonApp/Core/Constants/AppConstants.swift
@@ -10,7 +10,7 @@ enum AppConstants {
     
     enum API {
 
-        static let baseURL = "http://192.168.3.8:3001"
+        static let baseURL = "https://wsios-2.onrender.com"
 
         static let imageBasePath = baseURL + "/images/"
         static let timeout: TimeInterval = 30
